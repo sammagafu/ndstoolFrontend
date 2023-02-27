55e1c02003e7c55d4bdf4aab15384e29e52df990
Samuel Magafu <hi@sammagafu.com>
2023-02-27 15:52:06 +0300

    fixed and added login pages and register page

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/Home.vue')
+    },
+    {
+      path:'/account',
+      children : [
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('../views/Register.vue')
+        },
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('../views/Login.vue')
+        }
+      ]
+    }
+  ]
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TopNavigationBar from '../components/TopNavigationBar.vue';
+</script>
+<template>
+   <TopNavigationBar></TopNavigationBar>
+    <section class="py-12">
+        <h1>Welcome ...</h1>
+    </section>
+</template>
