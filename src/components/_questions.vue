4e1b3874833f642debe7d57a1d45bc7975c17f2f
jaquesony <jacksonmwatuka@gmail.com>
2023-05-26 13:18:01 +0300

    questions fetched done by category

--- src/components/_questions.vue
+++ src/components/_questions.vue
@@ -1,46 +1,35 @@
 <template>
   <div>
     <!-- <TopNavigationBar/> -->
-    <div v-for="(question, index) in displayedQuestions" :key="index" class="m-4">
-      <v-for v-for="(quest, ind) in question.questions" :key="ind">
-        <h3 v-if="question.category === 'Fine Motor'" class="text-xl font-semibold tracking-wide break-all">
-          {{ (currentPage - 1) * itemsPerPage + ind + 1 }}. {{ quest.question }}
-        </h3>
-        <div
-          
-          class="bg-slate-200 hover:bg-cyan-600 m-2"
-        >
-          <label :for="'true' + index">
-            <input
-              type="radio"
-              :name="quest.question"
-              :id="'true' + index"
-              value="true"
-              v-model="quest.userAnswer"
-            />
-            True
-          </label>
-        </div>
-        <div class="bg-slate-200 hover:bg-cyan-600 m-2">
-          <label :for="'false' + index">
-            <input
-              type="radio"
-              :name="quest.question"
-              :id="'false' + index"
-              value="false"
-              v-model="quest.userAnswer"
-            />
-            False
-          </label>
-        </div>
-      </v-for>
+    <div v-for="(question, index) in questions" :key="index" class="m-4">
+      <div v-if="question.id === currentPage">
+        <h1 class=" text-3xl">{{ question.category }}</h1>
+        <v-for v-for="(quest, ind) in question.questions" :key="ind">
+          <h3 class="text-xl font-semibold tracking-wide break-all">
+            <!-- {{ (currentPage - 1) * question.questions.length + ind + 1 }}. -->
+             {{ quest.question }}
+          </h3>
+          <div class="bg-slate-200 hover:bg-cyan-600 m-2">
+            <label :for="'true' + index">
+              <input type="radio" :name="quest.question" :id="'true' + index" value="true" v-model="quest.userAnswer" />
+              True
+            </label>
+          </div>
+          <div class="bg-slate-200 hover:bg-cyan-600 m-2">
+            <label :for="'false' + index">
+              <input type="radio" :name="quest.question" :id="'false' + index" value="false" v-model="quest.userAnswer" />
+              False
+            </label>
+          </div>
+        </v-for>
+      </div>
     </div>
     <div class="flex justify-center my-8">
-      <button class="mx-2" @click="currentPage--" :disabled="currentPage === 1">
+      <button class="mx-2" @click="decrementPage()" :disabled="currentPage === 1">
         Previous
       </button>
-      <button class="mx-2" @click="currentPage++" :disabled="currentPage === pageCount">
-        Next
+      <button class="mx-2" @click="currentPage= currentPage+1" :disabled="currentPage === questions.length">
+        Next {{ currentPage }}/{{ questions.length }}
       </button>
     </div>
   </div>
@@ -51,8 +40,6 @@ import TopNavigationBar from "/src/components/TopNavigationBar.vue";
 import axios from "axios";
 import { onMounted, ref, computed } from "vue";
 
-const itemsPerPage = 12;
-
 onMounted(() => {
   axios.get("category").then((response) => {
     // const { data } = response;
@@ -60,7 +47,7 @@ onMounted(() => {
       el.userAnswer = "";
     });
     questions.value = response.data;
-    console.log("vvv", questions.value);
+    // console.log("vvv", questions.value);
   });
 });
 
@@ -68,11 +55,15 @@ const questions = ref([]);
 
 const currentPage = ref(1);
 
-const pageCount = computed(() => Math.ceil(questions.value.length / itemsPerPage));
+const decrementPage = () => {
+  currentPage.value--;
+};
 
-const displayedQuestions = computed(() => {
-  const startIndex = (currentPage.value - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  return questions.value.slice(startIndex, endIndex);
-});
+// const pageCount = computed(() => Math.ceil(questions.value.length / itemsPerPage));
+
+// const displayedQuestions = computed(() => {
+//   const startIndex = (currentPage.value - 1) * itemsPerPage;
+//   const endIndex = startIndex + itemsPerPage;
+//   return questions.value.slice(startIndex, endIndex);
+// });
 </script>
